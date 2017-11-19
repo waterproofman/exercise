d39464e18a61d7294bb12eba640a1d9396e63edf
waterproofman <pp.debski@gmail.com>
2017-11-19 15:02:57 +0100

    Passing ut for xcorr.

--- tests/xcorrTest.cpp
+++ tests/xcorrTest.cpp
@@ -1,17 +1,16 @@
 #include "gtest/gtest.h"
 #include "sdsp.hpp"
 
-TEST(xcorr, Positive)
+TEST(xcorr, Positive1)
 {
     std::vector<double> a = {0.1, 0.2, -0.1, 4.1, -2, 1.5, -0.1};
     std::vector<double> b = {0.1, 4, -2.2, 1.6, 0.1, 0.1, 0.2};
-    std::vector<double> out = {0.02, 0.05, 0.01, 0.99, 0.1, 0.31, 7.52, -12.46, 23.18, -11.05, 6.02, -0.25, -0.1};
+    std::vector<double> out = {0.02, 0.05, 0.01, 0.99, 0.1, 0.31, 7.52, -12.46, 23.18, -11.05, 6.02, -0.25, -0.01};
     std::vector<double> xc = xcorr(a,b);
     for(int i = 0; i < xc.size(); i++)
     {
-    	EXPECT_EQ(xc[i], out[i]);
+    	EXPECT_DOUBLE_EQ(xc[i], out[i]);
     }
-    EXPECT_EQ(xcorr(a,b), out);
-    EXPECT_EQ(xcorr(a,b).size(), 13);
+    EXPECT_EQ(xcorr(a,b).size(), a.size() + b.size() -1);
 }
 
