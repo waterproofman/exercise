7f2712da94b855b5f08b49651e633702b23fc804
waterproofman <pp.debski@gmail.com>
2017-11-19 14:44:43 +0100

    partial ut for xcorr

--- tests/gTestMain.cpp
+++ tests/gTestMain.cpp
@@ -1,4 +1,4 @@
-#include <limits.h>
+//#include <limits.h>
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
--- tests/xcorrTest.cpp
+++ tests/xcorrTest.cpp
@@ -1,8 +1,12 @@
 #include "gtest/gtest.h"
+#include "sdsp.hpp"
 
-TEST(testMath, myCubeTest)
+TEST(xcorr, Positive)
 {
-	volatile int i = 1000;
-    EXPECT_EQ(1000, i);
+    std::vector<double> a = {0.1, 0.2, -0.1, 4.1, -2, 1.5, -0.1};
+    std::vector<double> b = {0.1, 4, -2.2, 1.6, 0.1, 0.1, 0.2};
+    std::vector<double> out = {0.02, 0.05, 0.01, 0.99, 0.1, 0.31, 7.52, -12.46, 23.18, -11.05, 6.02, -0.25, -0.1};
+    EXPECT_EQ(xcorr(a,b), out);
+    EXPECT_EQ(xcorr(a,b).size(), 13);
 }
 
